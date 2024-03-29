db015a6bf752a35522194775d3ff4c65d2459168
Matt Mahoney <binbashing@gmail.com>
2024-03-28 20:50:03 -0400

    enrtypoint tweaks

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+set -e
+
 handle_sigterm() {
     echo "SIGTERM received, stopping Fail2Ban server..."
-    # Add any cleanup commands here before exiting
     /usr/bin/fail2ban-client stop
     exit 0
 }
@@ -12,18 +13,8 @@ trap handle_sigterm SIGTERM
 
 envsubst < /jail.conf | sponge /etc/fail2ban/jail.conf
 
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to substitute environment variables in jail.conf"
-    exit 1
-fi
-
 # Start Fail2Ban
 /usr/bin/fail2ban-server -f -x -v start
-
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to start Fail2Ban server"
-    exit 1
-fi
 
 # Keep script running to maintain control over the process and handle signals
 wait $!
